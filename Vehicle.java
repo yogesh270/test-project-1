ee79e5e96da11ea60921e884a30ef16aa6d31fa0
Yogesh <yogesh444980@gmail.com>
2025-11-15 22:05:15 +0530

    updated color white

--- Vehicle.java
+++ Vehicle.java
@@ -5,7 +5,7 @@ public class Vehicle {
     public static int vehicleCounter = 0;
     private String brand;
     public final String VIN;
-    private String color = "black";
+    private String color = "white";
 
     // constructor
     public Vehicle(String brand, String VIN) {
