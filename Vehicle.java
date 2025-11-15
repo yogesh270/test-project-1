d338ab46c158481af56353216002d8a2ac91e970
yogesh270 <yogesh444980@gmail.com>
2025-11-15 21:46:46 +0530

    added vehicle

---
+++ Vehicle.java
@@ -0,0 +1,26 @@
+// package vehicleFleetManager;
+
+public class Vehicle {
+    // initializations
+    public static int vehicleCounter = 0;
+    private String brand;
+    public final String VIN;
+
+    // constructor
+    public Vehicle(String brand, String VIN) {
+        this.brand = brand;
+        this.VIN = VIN;
+        vehicleCounter++;
+    }
+
+    // methods
+    public void startEngine() {
+        System.out.println("Vehicle engine starts!");
+
+    }
+
+    // getter
+    public String getBrand() {
+        return brand;
+    }
+}
