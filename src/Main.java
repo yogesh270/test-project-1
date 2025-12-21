6d0f47581cbb1fefded285acd5f67ed9e89d11f6
yogesh270 <yogesh444980@gmail.com>
2025-12-21 11:23:04 +0530

    added variable c with value 20

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String [] args){
         int a = 0;
         int b = 10;
+        int c = 20;
         for (int i = 1;i<10;i++){
             a+=i;
 
